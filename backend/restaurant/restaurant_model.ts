0a68b16c4ddc5d6f11c7541b1da8712c9148157f
Nathan Waskiewicz <Nathan.Waskiewicz@Leidos.com>
2022-11-06 13:38:33 -0500

    Google API to get restaurants from a hard coded location Then get an image of a restaurant based on results

--- backend/restaurant/restaurant_model.ts
+++ backend/restaurant/restaurant_model.ts
@@ -1,32 +1,33 @@
 // Imports
-
-// const axios = require('axios')
-// import * as fetch from "package_name";
-// import 'whatwg-fetch'
-const router = require('express').Router()
-module.exports = router
+require('dotenv').config();
 
 
-
-const key = 'ask Nate for this :P'
-console.log("get's here" + process.env.GOOGLE_API_KEY);
-// router.get('/restaurants', async (req, res, next) => {
-//  try {
+const key = process.env.GOOGLE_API_KEY;
+var img;
+console.log(key);
 fetch(
 `https://maps.googleapis.com/maps/api/place/nearbysearch/
-json?type=restaurant&location=-33.8670522%2C151.1957362&radius=1500&key=${key}`
+json?keyword=restaurant&location=28.453535%2C-81.293822&radius=1500&key=${key}`
 )
 .then((response) => response.json())
 .then((data) => {
-  console.log(data);
+  // console.log(data);
+  console.log(data.results[0].name);
+  console.log(data.results.length);
+  img = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=` +
+  data.results[0].photos[0].photo_reference + '&key=' + key;
+  console.log(img);
 })
-  //  } 
-//  catch (err) {
-  // next(err)
-// }
-// })
-
-console.log("also here");
+.then(() => {
+  fetch(img)
+  .then((response) => response)
+  .then((data) => {
+    // console.log(data);
+    // const myImage = new Image(100, 200);
+    // myImage.src = data;
+    // document.body.appendChild(myImage);
+  })
+})
 
 // Create schema of restaurant model
 
