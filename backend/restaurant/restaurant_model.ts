cfaf62be629cbd3c07abc273b35d82383c1f56fb
Nathan Waskiewicz <Nathan.Waskiewicz@Leidos.com>
2022-11-15 15:35:41 -0500

    Fetch restaurants hopefully works setup example restaurant routes model

--- backend/restaurant/restaurant_model.ts
+++ backend/restaurant/restaurant_model.ts
@@ -1,38 +1,24 @@
 // Imports
-require('dotenv').config();
+const mongoose = require('mongoose')
 
-
-const key = process.env.GOOGLE_API_KEY;
-var img;
-console.log(key);
-fetch(
-`https://maps.googleapis.com/maps/api/place/nearbysearch/
-json?keyword=restaurant&location=28.453535%2C-81.293822&radius=1500&key=${key}`
-)
-.then((response) => response.json())
-.then((data) => {
-  // console.log(data);
-  console.log(data.results[0].name);
-  console.log(data.results.length);
-  img = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=` +
-  data.results[0].photos[0].photo_reference + '&key=' + key;
-  console.log(img);
-})
-.then(() => {
-  fetch(img)
-  .then((response) => response)
-  .then((data) => {
-    // console.log(data);
-    // const myImage = new Image(100, 200);
-    // myImage.src = data;
-    // document.body.appendChild(myImage);
-  })
-})
 
 // Create schema of restaurant model
+const Schema = mongoose.Schema
 
-
-
-
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  place_id: {
+    type: Number,
+    required: true
+  },
+  reference: {
+    type: String,
+    required: true
+  }
+})
 
 // Export functions
+module.exports = mongoose.model('Workout', workoutSchema)
--- backend/restaurant/restaurant_routes.ts
+++ backend/restaurant/restaurant_routes.ts
@@ -1,18 +1,46 @@
 // Imports
-
+require('dotenv').config();
+const express = require('express')
+const router = express.Router()
+  
 
 // Create API routes for POST and GET requests
-
-
-
-
+const key = process.env.GOOGLE_API_KEY;
+var location = "28.600409241872445%2C-81.20164096376762"; //ucf
+var img;
+var radius = 1500;
 
 
 // Create function that fetches API data
+const fetchRestaurants = async(req, res) => {
+    const {location, radius} = req.params;
+fetch(
+`https://maps.googleapis.com/maps/api/place/nearbysearch/
+json?keyword=restaurant&location=${location}&radius=${radius}&key=${key}`
+)
+.then((response) => response.json())
+.then((data) => {
+//   console.log(data);
+  console.log("name of first restaurant in list:", data.results[0].name);
+  console.log("number of results:", data.results.length);
+  img = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=` +
+  data.results[0].photos[0].photo_reference + '&key=' + key;
+  console.log(img);
+})
+.then(() => {
+  fetch(img)
+  .then((response) => response)
+  .then((data) => {
+    // console.log(data);
+    // const myImage = new Image(100, 200);
+    // myImage.src = data;
+    // document.body.appendChild(myImage);
+  })
+})
+}
 
-
-
-
+router.get('/:location/:radius', fetchRestaurants)
 
 
 // Export functions
+module.exports = router
