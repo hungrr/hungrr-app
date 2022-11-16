b50d5100cfb042c832c57eba2d1378fde632683d
johnhuber3 <johnrhuber001@gmail.com>
2022-11-15 22:49:05 -0500

    started user routes and models

--- backend/user/user_model.ts
+++ backend/user/user_model.ts
@@ -1,11 +1,20 @@
-// Imports
-// Import this thing
 
+/*
+    NOT FINISHED - JUST A BASIC SKELETON TO BUILD OFF OF
+*/
+
+// Imports
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Scheme
+const restaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true})
+
+module.exports = mongoose.model('restaurant', restaurantSchema)
 
 // Create shcema of user model
-
-
-
-
-
 // Export functions
--- backend/user/user_routes.ts
+++ backend/user/user_routes.ts
@@ -1,10 +1,52 @@
+
+/*
+    NOT FINISHED - JUST A BASIC SKELETON TO BUILD OFF OF
+*/
+
 // Imports
 
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../models/user_models')
 
 // Create routes functions for GET, POST, UPDATE, and DELETE
 
+// GET
+
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET single'})
+})
 
 
+// POST
 
+router.post('/', (req, res) => {
+    const {title} = req.body
+    try {
+        const restaurant = await Restaurant.create({name})
+        res.status(200).json(restaurant)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+// DELETE
+
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE single'})
+})
+
+router.delete('/', (req, res) => {
+    res.json({mssg: 'DELETE all'})
+})
+
+// UPDATE
+
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE'})
+})
 
 // Export functions
+
+module.exports = router
+
