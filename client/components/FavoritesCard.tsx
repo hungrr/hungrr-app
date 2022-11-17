38c510e8e0eb72901d89fe76250f850650d8e3e7
khoivu166 <khoivu166@gmail.com>
2022-11-17 15:30:37 -0500

    Added ratings to the favorites page

--- client/components/FavoritesCard.tsx
+++ client/components/FavoritesCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Image, Text } from "react-native";
+import { StyleSheet, Image, Text, ImageBackground } from "react-native";
 import {
   VStack,
   Box,
@@ -8,20 +8,20 @@ import {
   ScrollView,
 } from "native-base";
 import { FavoritesCardProp } from "../Types and Interfaces/types";
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 
 export default function FavoritesCard(props:FavoritesCardProp){
   return (
     <Box style={styles.container}>
       <Box style={styles.picture}>
-        <Image
-          source={props.favorites.image}
-          style={{ flex: 1, borderRadius: 10 }}
-        />
+        <ImageBackground source={props.favorites.image} style={{ flex: 1 }}>
+          <Text style={styles.rating}>{props.favorites.rating}</Text>
+          <Ionicons style={styles.star} name={'star'} color={'gold'} size={13}/>
+        </ImageBackground>
       </Box>
       <Box style={styles.info}> 
         <Text style={styles.Description}>{props.favorites.name}</Text>
-        <Text>{props.favorites.rating}</Text>
       </Box>
     </Box>
   );
@@ -37,6 +37,8 @@ const styles = StyleSheet.create({
   picture: {
     flex: 1.5,
     backgroundColor: "transparent",
+    overflow: 'hidden',
+    borderRadius: 10,
   },
   info: {
     flex: 1,
@@ -49,10 +51,20 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     alignItems: "center",
+    top: 5,
+    left: 6,
   },
   rating: {
-    
-  }
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+    top: 90,
+    left: 300,
+  },
+  star: {
+    top: 73,
+    left: 330
+  },
 });
 
 
