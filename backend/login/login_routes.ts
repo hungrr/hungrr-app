996d5dad1375215bf952752ef9de680187d6afed
MasWill <mason@Masons-MacBook-Air.local>
2022-11-17 14:46:59 -0500

    finished most of server file, completed 2fa routes

--- backend/login/login_routes.ts
+++ backend/login/login_routes.ts
@@ -1,6 +1,61 @@
-import express from 'express'
+import express from 'express';
+import bodyparser from 'body-parser';
+import dotenv from 'dotenv';
+import initMB from 'messagebird';
+const router = express.Router();
+
+dotenv.config()
+let key: string
+if (process.env.MESSAGEBIRD_API_KEY) {
+  key = process.env.MESSAGEBIRD_API_KEY
+} else {
+  throw new Error("WHATEVER environment variable is not set")
+}
+
+const messagebird = initMB(key);
+
 
 
 
 // Phone number POST request
+
+router.post('/phonenumber', function(req, res) {
+    const number = req.body.number;    
+    messagebird.verify.create(number, {
+        originator : 'Code',
+        template : 'Your verification code is %token.'
+    }, function (err: any, response: any) {
+        if (err) {
+            console.log(err);
+            res.render('step1', {
+                error : err.errors[0].description
+            });
+        } else {
+            console.log(response);
+            res.render('step2', {
+                id : response.id
+            });
+        }
+    })    
+});
+
+router.post('/verify', function(req, res) {
+    const id = req.body.id;
+    const token = req.body.token;
+    messagebird.verify.verify(id, token, function(err: any, response: any) {
+        if (err) {
+            console.log(err);
+            res.render('step2', {
+                error: err.errors[0].description,
+                id : id
+            });
+        } else {
+            console.log(response);
+            res.render('step3');
+        }
+    })    
+});
+
+export default router;
+
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import bodyparser from 'body-parser';
+import loginRoutes from './login/login_routes';
 
 dotenv.config();
 
@@ -7,6 +9,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(bodyparser.urlencoded({ extended : true }));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
@@ -15,6 +18,26 @@ app.use((req, res, next) => {
 
 
 // routes
-//app.use('/',)
+app.use('/api/login',loginRoutes)
+app.use('api/user',)
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
+})
 
 // Connect to database
+/*
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
+*/
+
