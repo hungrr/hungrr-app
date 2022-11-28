8afa378c5d14b950ce8d1fc28e26d5c3999627e9
zeyf <zainy0012@gmail.com>
2022-11-28 04:51:11 -0500

    PAssing in the favorites and set favorites props into the component view to be able to keep persistent when swiping right on cards in card container component. need to add remove from favorites functionality

--- client/screens/Favorites.tsx
+++ client/screens/Favorites.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {StyleSheet, Image, Text} from 'react-native';
 import { VStack, Box, Center, NativeBaseProvider, ScrollView } from 'native-base';
 import FavoritesCard from '../components/FavoritesCard';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Profile } from '../Types and Interfaces/types';
 
-export default function Favorites() {
+export default function Favorites({ setFavorites, favorites }:any) {
+
     return (
         <NativeBaseProvider>
             <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center', padding: 25 }}>
-                {/* <FavoritesCard {...} /> */}
+                {
+                    (
+                        favorites.length === 0 ?
+                            <Text>
+                                No favorites... start swiping!
+                            </Text>
+                        :
+                        favorites.map((card:Profile) => <FavoritesCard {...card} />)
+                    )
+                }
             </ScrollView>
         </NativeBaseProvider>
     )
