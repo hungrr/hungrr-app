db0ebf2c12bc340a5229bca824f50cc9f5978f34
zeyf <zainy0012@gmail.com>
2022-11-27 21:55:24 -0500

    Added the functionality with AsyncStorage to locally store whether or not a user is logged in based on checking a key. If so, go in. is not, go out.

--- client/App.tsx
+++ client/App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Login from './screens/Login';
 import Navigation from "./navigation/MainContainer";
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function App() {
@@ -11,11 +11,23 @@ export default function App() {
   const [ loginState, setLoginState ] = useState({
     attemptedLogin: false,
     loggedIn: false,
-    loginView: -1
+    loginView: -1,
+    phoneNumber: "",
+    name: ""
   });
 
-  useEffect(() => {
+  useEffect(async () => {
+    try {
 
+      const isLoggedIn = await AsyncStorage.getItem("loggedIn");
+      if (isLoggedIn !== null) {
+        setLoginState({ ...loginState, loginView: 1 });
+      } else {
+        setLoginState({ ...loginState, loginView: -1 });
+      }
+    } catch(err) {
+      console.log("There has been an error checking logged in status");
+    }
   }, [  ]);
 
   return (
