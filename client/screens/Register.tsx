d22cd17e5a9040d4cd3597a42d53cc501b3143ae
khoivu166 <khoivu166@gmail.com>
2022-11-29 00:05:39 -0500

    Styled Register page

--- client/screens/Register.tsx
+++ client/screens/Register.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect }  from "react";
-import { View, Text, TextInput, Pressable, Button } from "react-native";
+import { View, Text, TextInput, Pressable, Button, StyleSheet, Image } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from "axios";
 
@@ -22,18 +22,22 @@ export default ({ setLoginState, loginState }:any) => {
     };
 
     return (
-        <View>
-            <Pressable>
-                <Button title="Login" onPress={() => {
-                    setLoginState({ ...loginState, loginView: -1 })
-                }} />
-            </Pressable>
-            <Text>Register</Text>
-            <View>
-                <TextInput placeholder="Enter Name" onChangeText={(text:string) => setLoginState({ ...loginState, name: text })} value={loginState.name} />
-                <TextInput placeholder="Enter Phone Number" onChangeText={(text:string) => setLoginState({ ...loginState, phoneNumber: text })} value={loginState.phoneNumber} />
-                <Pressable>
-                    <Button title="Submit" onPress={() => {
+        <View style={styles.container}>
+            <View style={styles.loginContainer}>
+                <Pressable style={styles.login}>
+                    <Button color='black' title="Login" onPress={() => {
+                        setLoginState({ ...loginState, loginView: -1 })
+                    }} />
+                </Pressable>
+            </View>
+
+
+            <View style={styles.inputContainer}>
+                <Text style={styles.registerText}>Register</Text>
+                <TextInput style={styles.nameInput} placeholder="     Enter Name" onChangeText={(text:string) => setLoginState({ ...loginState, name: text })} value={loginState.name} />
+                <TextInput style={styles.numberInput} placeholder="     Enter Phone Number" onChangeText={(text:string) => setLoginState({ ...loginState, phoneNumber: text })} value={loginState.phoneNumber} />
+                <Pressable style={styles.submit}>
+                    <Button color='black' title="Submit" onPress={() => {
                         registerUser();
                     }} />
                 </Pressable>
@@ -43,6 +47,49 @@ export default ({ setLoginState, loginState }:any) => {
 
 };
 
-const styles = {
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EE8980',
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    submit: {
+        width: '20%'
+    },
+    login: {
+        width: '20%',
+        margin: 10,
+    },
+    loginContainer: {
+        flex: 0.1,
+        width: '100%',  
+        
+    },
+    registerText: {
+        fontSize: 30,
+        fontWeight: 'bold',
 
-};
+    },
+    inputContainer: {
+        flex: 0.8,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    nameInput: {
+        backgroundColor: 'white',
+        width: '50%',
+        margin: 10,
+        borderRadius: 10,
+        padding: 10
+    },
+    numberInput: {
+        backgroundColor: 'white',
+        width: '50%',
+        margin: 10,
+        borderRadius: 10,
+        padding: 10
+    }
+    
+})
